9ed099b1d7597ed3893703e2bb72c0a1d76d4a46
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-20 05:45:45 +0700

    Создает шаблон

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-//
+import galleryCardTemp from '../src/templates/gallery-card.hbs';
+import cards from './menu.json';
+import './styles.css';
+
+const galleryRef = document.querySelector('.js-menu');
+
+const markup = galleryCardTemp(cards);
+galleryRef.insertAdjacentHTML('beforeend', markup);
