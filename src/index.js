2ad6dc8b0c73688c1f57ca9b0e7e48c6ef3c0951
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-25 01:32:46 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ const galleryRef = document.querySelector('.js-menu');
 
 const markup = galleryCardTemp(cards);
 galleryRef.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const userJSON = Json.stringify(Theme);
+
+localStorage.setItem('my-key', userJSON);
+
+//const getValue = localStorage.getItem('my-key');
