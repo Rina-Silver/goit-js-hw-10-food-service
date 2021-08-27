719027d7f8298dbc0704679dcd30831e66c393f4
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-27 22:27:44 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import galleryCardTemp from '../src/templates/gallery-card.hbs';
 import cards from './menu.json';
 import './styles.css';
 
-const galleryRef = document.querySelector('.js-menu');
-
-const markup = galleryCardTemp(cards);
-galleryRef.insertAdjacentHTML('beforeend', markup);
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-const userJSON = Json.stringify(Theme);
+//Отрисовка карточек
+const galleryRef = document.querySelector('.js-menu');
 
-localStorage.setItem('my-key', userJSON);
+const markup = galleryCardTemp(cards);
+galleryRef.insertAdjacentHTML('beforeend', markup);
 
-//const getValue = localStorage.getItem('my-key');
+//Изменение темы
+const themeToggle = document.getElementById('theme-switch-toggle');
+
+themeToggle.addEventListener('change', handleThemeChange);
+function handleThemeChange() {
+  const ToggleCheck = themeToggle.checked;
+
+  if (ToggleCheck === false) {
+    document.body.classList.add(Theme.LIGHT);
+    console.log(Theme.LIGHT);
+    //localStorage.setItem('myTheme', JSON.stringify(Theme.LIGHT));
+  }
+  document.body.classList.add(Theme.DARK);
+  console.log(Theme.DARK);
+}
+
+//const userJSON = JSON.stringify(Theme);
+
+//localStorage.setItem('myTheme', userJSON);
+
+//const getValue = localStorage.getItem('myTheme');
