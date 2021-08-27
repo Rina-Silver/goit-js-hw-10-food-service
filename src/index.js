478fc463c12b3b47482df69c77427d7fd7bdaf92
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-27 23:17:13 +0700

    Update index.js
    работа с чекбоксом и localStorage

--- src/index.js
+++ src/index.js
@@ -21,16 +21,20 @@ function handleThemeChange() {
   const ToggleCheck = themeToggle.checked;
 
   if (ToggleCheck === false) {
-    document.body.classList.add(Theme.LIGHT);
-    console.log(Theme.LIGHT);
-    //localStorage.setItem('myTheme', JSON.stringify(Theme.LIGHT));
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('myTheme', Theme.LIGHT);
+  } else {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('myTheme', Theme.DARK);
   }
-  document.body.classList.add(Theme.DARK);
-  console.log(Theme.DARK);
 }
 
-//const userJSON = JSON.stringify(Theme);
-
-//localStorage.setItem('myTheme', userJSON);
-
-//const getValue = localStorage.getItem('myTheme');
+//При загрузке страницы
+const getValue = localStorage.getItem('myTheme');
+if (getValue === Theme.DARK) {
+  document.body.classList.add(Theme.DARK);
+  themeToggle.checked = true;
+} else {
+  document.body.classList.add(Theme.LIGHT);
+  themeToggle.checked = false;
+}
